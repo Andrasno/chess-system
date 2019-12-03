fdb2df448f392eb94ea78a1fbe9f7af2130bd106
Andre Torres <andrasno@gmail.com>
2019-12-03 07:01:16 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,14 +1,14 @@
 package boardgame;
 
 public class Board {
-	private Piece[][] piece;
+	private Piece[][] pieces;
 	private int rows;
 	private int columns;
 	
 	public Board(int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
-		piece = new Piece[rows][columns];
+		pieces = new Piece[rows][columns];
 	}
 
 	public int getRows() {
@@ -28,12 +28,18 @@ public class Board {
 	}
 	
 	public Piece piece(int row, int column) {
-		return piece[row][column];
+		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
-		return piece[position.getRow()][position.getColumn()];
+		return pieces[position.getRow()][position.getColumn()];
 	}
+	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
+	
 	
 	
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,12 +1,15 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	private Board board;
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -17,5 +20,9 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+	
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
 	}
 }
